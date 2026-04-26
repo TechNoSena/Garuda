5fe19ac527e0fbc105865e3fbbaa09a10d67edd7
Vedk020 <komrelliwarv@gmail.com>
2026-04-27 01:19:53 +0530

    fix: remove ndkVersion to fix build action and update setup-java

--- flutter_app/android/app/build.gradle.kts
+++ flutter_app/android/app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
 android {
     namespace = "com.garuda.garuda_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "25.1.8937393"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
